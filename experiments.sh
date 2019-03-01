e4f28ca74bbce1c0c8ea71163e82ffcd4cc67fe3
Fábio Maia <fabiu.maia@gmail.com>
2019-03-01 21:15:25 +0000

    Move experiments script

--- experiments.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -euxo pipefail
-
-for i in 21 17 13 9 5 2; do
-    rm -rf $(pwd)/out/experiment_$i/
-    mkdir -p $(pwd)/out/experiment_$i/tensorboard
-    python src/train.py --images_path /Volumes/data/images/ --descriptions_path /Volumes/data/descriptions/ --experiment $(pwd)/out/experiment_$i/ --nb_layers $i --augmentation
-done
