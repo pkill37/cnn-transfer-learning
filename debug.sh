9e54ab3dc55ceb6c4378c1d5843b0124736a4830
Fábio Maia <fabiu.maia@gmail.com>
2019-03-26 01:04:11 +0000

    Add debug script

---
+++ debug.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+rm -rf ./experiments/test/
+
+python ./src/train.py --experiments-path ./experiments/test/ \
+                      --train ./data/train_vgg16.npz \
+                      --validation ./data/validation_vgg16.npz \
+                      --pretrained-model vgg16 \
+                      --extract-until 18 \
+                      --freeze-until 18 \
+                      --epochs 1000 \
+                      --batch-size 32 \
+                      --lr 0.01 \
+                      --l1 0.01 \
+                      --l2 0.01
