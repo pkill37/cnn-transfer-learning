37f702ec4e412b9385ac66f57b5876cff73ed042
Fábio Maia <fabiu.maia@gmail.com>
2019-04-17 19:00:01 +0000

    Fix test script

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ pip install -r requirements.txt
 
 ## Data
 
-Download the datasets from the [ISIC 2017](https://challenge.kitware.com/#challenge/n/ISIC_2017%3A_Skin_Lesion_Analysis_Towards_Melanoma_Detection) Part 3 challenge and preprocess them to obtain the final compressed datasets:
+1. Download the datasets from the [ISIC 2017](https://challenge.kitware.com/#challenge/n/ISIC_2017%3A_Skin_Lesion_Analysis_Towards_Melanoma_Detection) Part 3 challenge.
+
+2. Preprocess them to obtain the final compressed datasets.
 
 ```
 ./data.sh
@@ -30,4 +32,10 @@ Monitor training:
 
 ```
 ./monitor.sh
+```
+
+## Test
+
+```
+python ./src/test.py --model ./experiments/debug_1554919063/model.hdf5 --test ./data/vgg19_test/vgg19_test.npz
 ```
--- src/test.py
+++ src/test.py
@@ -1,50 +1,37 @@
 import os
-import multiprocessing
 import argparse
+
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
+
 import data
 import helpers
 import metrics
 
 
 if __name__ == '__main__':
-    helpers.seed()
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--images-path', type=str)
-    parser.add_argument('--descriptions-path', type=str)
-    parser.add_argument('--model', type=str)
-    parser.add_argument('--img-height', type=int)
-    parser.add_argument('--img-width', type=int)
-    parser.add_argument('--batch-size', type=int)
+    parser.add_argument('--model', type=helpers.is_file, required=True)
+    parser.add_argument('--test', type=helpers.is_file, required=True)
     args = parser.parse_args()
 
-    # Load best trained model
     model = tf.keras.models.load_model(args.model, custom_objects={
-        'precision': metrics.precision(),
-        'recall': metrics.recall(),
-        'f1_score': metrics.f1_score()
+        'p': metrics.precision(),
+        'r': metrics.recall(),
+        'f1': metrics.f1_score(),
+        'tp': metrics.true_positive(),
+        'tn': metrics.true_negative(),
+        'fp': metrics.false_positive(),
+        'fn': metrics.false_negative(),
     })
 
-    # Build test generator
-    _, __, test_generator = data.generators(
-        images_path=args.images_path,
-        descriptions_path=args.descriptions_path,
-        img_height=args.img_height,
-        img_width=args.img_width,
-        split=(0.8, 0.1, 0.1),
-        batch_size=args.batch_size,
-        augmentation=False,
+    x_test, y_test = data.load(args.test)
+
+    scores = model.evaluate(
+        x=x_test,
+        y=y_test,
+        batch_size=32,
+        verbose=1,
     )
 
-    # Evaluate model on test generator
-    scores = model.evaluate_generator(
-        generator=test_generator,
-        verbose=1,
-        workers=multiprocessing.cpu_count()-1 or 1,
-        use_multiprocessing=True,
-    )
     for score, metric in zip(scores, model.metrics_names):
         print("%s: %0.4f" % (metric, score))
