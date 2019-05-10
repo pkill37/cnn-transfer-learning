20ec91b200bb0266d2388ff6a85238ea2e477022
Fábio Maia <fabiu.maia@gmail.com>
2019-05-10 18:29:27 +0000

    Refactor vgg19 train script

--- src/vgg19.py
+++ src/vgg19.py
@@ -4,27 +4,29 @@ import argparse
 import tensorflow as tf
 import numpy as np
 
-import metrics
 import data
 import helpers
 
 
 LOSS = 'binary_crossentropy'
-METRICS = metrics.METRICS
-OPTIMIZER = lambda lr: tf.keras.optimizers.SGD(lr=lr, momentum=0.9, decay=10e-6, nesterov=True)
+METRICS = ['accuracy']
+OPTIMIZER = tf.keras.optimizers.SGD(lr=10e-2, momentum=0.9, decay=0.0, nesterov=False)
+LR_DECAY = 0.1
+L2 = 5*10e-4
+DROPOUT = 0.5
+BS = 256
+
 
 IMG_WIDTH = 224
 IMG_HEIGHT = 224
 IMG_CHANNELS = 3
 
 
-def vgg19(extract_until=21, freeze_until=21, lr=0.001, l2=0.001):
+def vgg19(extract_until=21, freeze_until=21):
     assert extract_until >= freeze_until
 
     # Extract and freeze pre-trained model layers
     vgg19 = tf.keras.applications.vgg19.VGG19(weights='imagenet', input_shape=(IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS), include_top=False)
-    vgg19.summary()
-
     model = tf.keras.models.Sequential()
     for i in range(0, extract_until+1): # i=0 is the input layer, i>0 are the actual model layers
         layer = vgg19.layers[i]
@@ -33,20 +35,23 @@ def vgg19(extract_until=21, freeze_until=21, lr=0.001, l2=0.001):
 
     # Classifier
     model.add(tf.keras.layers.GlobalAveragePooling2D())
-    model.add(tf.keras.layers.Dense(units=512, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(l2)))
+    # TODO: is this regularizing the cost function?
+    model.add(tf.keras.layers.Dense(units=512, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(L2)))
+    model.add(tf.keras.layers.Dropout(rate=DROPOUT))
     model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
-    model.compile(loss=LOSS, optimizer=OPTIMIZER(lr), metrics=METRICS)
+    model.compile(loss=LOSS, optimizer=OPTIMIZER, metrics=METRICS)
     return model
 
 
-def train(experiment, train, extract_until, freeze_until, lr, l2, epochs, bs):
-    model = vgg19(extract_until, freeze_until, lr, l2)
+def train(experiment, train, extract_until, freeze_until, epochs, bs):
+    model = vgg19(extract_until, freeze_until)
     model.summary()
 
     callbacks = [
+        tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-2, patience=20, verbose=1, mode='min', baseline=None),
+        tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=LR_DECAY, patience=10, verbose=1, mode='min', min_delta=0.0001, cooldown=0, min_lr=0),
         tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(experiment, 'model.h5'), monitor='loss', verbose=0, save_best_only=True, save_weights_only=False, mode='min', period=1),
-        tf.keras.callbacks.TensorBoard(log_dir=experiment, histogram_freq=0, write_graph=True, write_images=True),
-        tf.keras.callbacks.CSVLogger(filename=os.path.join(experiment, 'log.csv'), separator=',', append=False),
+        tf.keras.callbacks.CSVLogger(filename=os.path.join(experiment, 'train.csv'), separator=',', append=False),
     ]
 
     x_train, y_train = data.load(train)
@@ -69,11 +74,9 @@ if __name__ == '__main__':
     parser.add_argument('--train', type=helpers.is_file, required=True)
     parser.add_argument('--extract-until', type=int, required=True)
     parser.add_argument('--freeze-until', type=int, required=True)
-    parser.add_argument('--lr', type=float, required=True)
-    parser.add_argument('--l2', type=float, required=True)
     parser.add_argument('--epochs', type=int, required=True)
     parser.add_argument('--bs', type=int, required=True)
     args = parser.parse_args()
 
-    train(args.experiment, args.train, args.extract_until, args.freeze_until, args.lr, args.l2, args.epochs, args.bs)
+    train(args.experiment, args.train, args.extract_until, args.freeze_until, args.epochs, args.bs)
     helpers.fix_layer0(os.path.join(args.experiment, 'model.h5'), (None, IMG_WIDTH, IMG_HEIGHT, IMG_CHANNELS), 'float32')
--- vgg19.sh
+++ vgg19.sh
@@ -2,18 +2,23 @@
 set -euo pipefail
 . ./env/bin/activate
 
+vgg19=$(echo ./experiments/vgg19)
+rm -rf $vgg19
+
 for extract in 21 16 11 6 3; do
-    for freeze in 21 16 11 6 3; do
-        experiment=$(echo ./experiments/vgg19_"$extract"_"$freeze"/)
-        rm -rf $experiment && mkdir -p $experiment && echo $experiment
+    for freeze in 21 16 11 6 3 0; do
+        if [ "$extract" -lt "$freeze" ]; then
+            continue
+        fi
+
+        experiment=$(echo $vgg19/extract"$extract"_freeze"$freeze")
+        mkdir -p $experiment && echo $experiment
 
         python ./src/vgg19.py --experiment $experiment \
                               --train ./data/isic2018/224/train/train.npz \
                               --extract-until $extract \
                               --freeze-until $freeze \
-                              --lr 0.0001 \
-                              --l2 0.0001 \
-                              --epochs 300 \
+                              --epochs 1000 \
                               --bs 64
 
         python ./src/test.py --model $experiment/model.h5 \
