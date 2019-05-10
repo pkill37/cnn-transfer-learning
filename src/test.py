0e4904f2d9a23d0ca3b9537c3517206951d6a99c
Fábio Maia <fabiu.maia@gmail.com>
2019-05-10 18:04:14 +0000

    Refactor test script

--- src/test.py
+++ src/test.py
@@ -1,11 +1,29 @@
 import os
 import argparse
+import json
 
 import tensorflow as tf
+import numpy as np
 
 import data
 import helpers
-import metrics # imports metrics such that model can be loaded
+
+
+def test(model, test):
+    m = tf.keras.models.load_model(model)
+    x_test, y_test = data.load(test)
+
+    y_pred = m.predict(
+        x=x_test,
+        batch_size=32,
+        verbose=1,
+    )
+
+    y_pred.reshape(-1)
+    y_pred[y_pred <= 0.5] = 0.
+    y_pred[y_pred > 0.5] = 1.
+
+    np.savez_compressed(os.path.join(os.path.dirname(model), 'predictions'), y_true=y_test, y_pred=y_pred)
 
 
 if __name__ == '__main__':
@@ -14,16 +32,4 @@ if __name__ == '__main__':
     parser.add_argument('--test', type=helpers.is_file, required=True)
     args = parser.parse_args()
 
-    model = tf.keras.models.load_model(args.model)
-
-    x_test, y_test = data.load(args.test)
-
-    scores = model.evaluate(
-        x=x_test,
-        y=y_test,
-        batch_size=32,
-        verbose=1,
-    )
-
-    for score, metric in zip(scores, model.metrics_names):
-        print("%s: %0.4f" % (metric, score))
+    test(args.model, args.test)
