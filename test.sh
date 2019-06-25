3d2ea3a0a1e646076699b2040c10a428e63c279c
Fábio Maia <fabio.maia@ua.pt>
2019-06-25 16:26:13 -0400

    Improve test/plot

--- test.sh
+++ test.sh
@@ -2,9 +2,5 @@
 set -euo pipefail
 
 experiments=$(echo "$1")
-mkdir -p $experiments
 
-for experiment in $(find $experiments -mindepth 1 -maxdepth 1 -type d); do
-    python ./src/test.py --model $experiment/model.h5 --test-set ./data/isic2018/224/test/test.npz
-    python ./src/plot.py --experiment $experiment
-done
+python ./src/test.py --experiments $experiments --test-set ./data/isic2018/224/test/test.npz
