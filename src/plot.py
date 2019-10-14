44a80ae4b331827ed8ef350bcd65cdb9a7ce69a4
Fábio Maia <fabio.maia@ua.pt>
2019-10-13 22:43:22 -0400

    Add code to tabulate results

--- src/plot.py
+++ src/plot.py
@@ -28,20 +28,87 @@ def plot_train_loss(stat, target_file):
     plt.savefig(target_file)
 
 
+def truncate_floats(stats):
+    def truncate(f):
+        return float("{:.4f}".format(f))
+
+    for stat in stats:
+        for h in stat['hyperparameters']:
+            if isinstance(stat['hyperparameters'][h], float):
+                stat['hyperparameters'][h] = truncate(stat['hyperparameters'][h])
+        stat['train_acc'][-1] = truncate(stat['train_acc'][-1])
+        stat['val_acc'][-1] = truncate(stat['val_acc'][-1])
+        stat['test_acc'] = truncate(stat['test_acc'])
+    return stats
+
+
+def tabulate(stats, target_file):
+    assert len(stats) > 0
+
+    """
+    \begin{center}
+    \begin{tabular}{ |c|c|c| }
+     \hline
+     cell1 & cell2 & cell3 \\
+     cell4 & cell5 & cell6 \\
+     cell7 & cell8 & cell9 \\
+     \hline
+    \end{tabular}
+    \end{center}
+    """
+
+    # Truncate floats to 2 decimal places for presentation purposes
+    stats = truncate_floats(stats)
+
+    # Build header
+    latex = '\\begin{table}[ht]\n'
+    latex += '\\centering\n'
+    latex += '\\begin{tabular}{ '
+    latex += '|c'*(len(stats[0]['hyperparameters']) + 3) + '|'
+    latex += ' }\n'
+    latex += '\\hline\n'
+    for h in stats[0]['hyperparameters']:
+        latex += f"{h} & "
+    latex += "train acc & val acc & test acc \\\\\n"
+    latex += '\\hline\n'
+
+    # Build body
+    for stat in stats:
+        for h in stat['hyperparameters']:
+            latex += f"{stat['hyperparameters'][h]} & "
+        latex += f"{stat['train_acc'][-1]} & {stat['val_acc'][-1]} & {stat['test_acc']} \\\\\n"
+    latex += '\\hline\n'
+    latex += '\\end{tabular}\n'
+    latex += '\\caption{Foobar}\n'
+    latex += '\\label{table:foobar}\n'
+    latex += '\\end{table}\n'
+
+    print(latex)
+
+    with open(target_file, 'w') as f:
+        f.write(latex)
+
+
 def main(experiments, plots):
     stats = os.path.join(experiments, 'stats.json')
     with open(stats) as f:
         stats = json.loads(f.read())
 
-    # Pick top 10 sorted by F1
-    stats = sorted(stats, key=lambda x: x['classification_report']['weighted avg']['f1-score'])
-    stats = stats[:10]
+    # Sort by F1
+    stats = sorted(stats, key=lambda x: x['classification_report']['weighted avg']['f1-score'], reverse=True)
+    stats = stats[:20]
 
+    # Tabulate results
+    tabulate(stats, os.path.join(plots, 'table.tex'))
+
+    # Plot graphs
+    """
     for i, stat in enumerate(stats):
         target_path = os.path.join(plots, stat['id'])
         helpers.create_or_recreate_dir(target_path)
         target_file = os.path.join(target_path, f'{i}_cost.png')
         plot_train_loss(stat, target_file)
+    """
 
 
 if __name__ == '__main__':
