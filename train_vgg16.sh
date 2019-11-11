68370b16d512023c054eeac6e7abddea50d0254d
Fábio Maia <fabio.maia@ua.pt>
2019-11-11 14:25:50 -0500

    Update parameters in train script

--- train_vgg16.sh
+++ train_vgg16.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 set -euxo pipefail
 
-experiments=$(echo ./experiments_fixed/)
+experiments=$(echo ./experiments/vgg16)
 mkdir -p $experiments
 
-#for extract in 18 14 10 06 03; do
-	#for freeze in 18 14 10 06 03 00; do
-for extract in 18; do
-	for freeze in 18; do
+for extract in 18 14 10 06 03; do
+	for freeze in 00 10 06 14 03 18; do
+		# ensure extract >= freeze
 		if [ "$extract" -lt "$freeze" ]; then
 			continue
 		fi
 
-		for lambda in 1.00000000e-04 2.78255940e-04 7.74263683e-04 2.15443469e-03 5.99484250e-03 1.66810054e-02 4.64158883e-02 1.29154967e-01 3.59381366e-01 1.00000000e+00; do
+		# np.geomspace(0.0001, 0.005, 10)
+		for lambda in 0.0001 0.00015445 0.00023853 0.0003684 0.00056898 0.00087876 0.00135721 0.00209614 0.00323739 0.005; do
 			python ./src/train_vgg16.py \
 				--experiments "$experiments" \
 				--train ./data/isic2018/224/train/train.npz \
@@ -23,7 +23,7 @@ for extract in 18; do
 				--units 512 \
 				--l2 "$lambda" \
 				--dropout 0.0 \
-				--patience 10
+				--patience 20
 		done
 	done
 done
